bc5911191665992c1d18af9c4a3d058eddab94e6
JN GLX <jn.glx@outlook.fr>
2022-07-03 22:11:06 +0200

    Ajout des tests unitaires

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
 """
 Module Calculatrice simple pour test de Circle CI
 """
+
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
-
