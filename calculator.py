ebdb86dfb45b77664b6c4aa46eac245557ec2079
JN GLX <jn.glx@outlook.fr>
2022-07-03 22:00:14 +0200

    Ajout des fonctions pour l'addition et la soustraction

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Module Calculatrice simple pour test de Circle CI
+"""
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
